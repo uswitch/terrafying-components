6515270728f9e6973fe964bb9a104ff92810db0b
Tom Booth <tombooth@gmail.com>
2018-03-14 18:20:05 +0000

    Allow usable components to be exposed to ICMP pings
    Sometimes we want to let other services or CIDRs ping us.

--- spec/support/shared_examples/usable.rb
+++ spec/support/shared_examples/usable.rb
@@ -2,6 +2,8 @@ shared_examples "a usable resource" do
 
   it { is_expected.to respond_to(:used_by) }
   it { is_expected.to respond_to(:used_by_cidr) }
+  it { is_expected.to respond_to(:pingable_by) }
+  it { is_expected.to respond_to(:pingable_by_cidr) }
 
   let :ports do
     [
@@ -42,6 +44,37 @@ shared_examples "a usable resource" do
 
   end
 
+  it "should add icmp that maps the right cidrs" do
+    cidrs = ["10.1.0.0/16", "10.2.0.0/16"]
+    @main_resource.pingable_by_cidr(*cidrs)
+
+    output = @main_resource.output_with_children
+
+    expect(output["resource"]["aws_security_group_rule"].count).to be >= 2
+
+    expect(
+      output["resource"]["aws_security_group_rule"].any? { |_, rule|
+        rule[:type] == "ingress" && \
+        rule.has_key?(:cidr_blocks) && \
+        rule[:cidr_blocks] == cidrs && \
+        rule[:from_port] == 8 && \
+        rule[:to_port] == 0 && \
+        rule[:protocol] == 1
+      }
+    ).to be true
+
+    expect(
+      output["resource"]["aws_security_group_rule"].any? { |_, rule|
+        rule[:type] == "ingress" && \
+        rule.has_key?(:cidr_blocks) && \
+        rule[:cidr_blocks] == cidrs && \
+        rule[:from_port] == 128 && \
+        rule[:to_port] == 0 && \
+        rule[:protocol] == 58
+      }
+    ).to be true
+  end
+
   it "should add ingress that maps the right resources" do
     other_resource = Terrafying::Components::Instance.create_in(@vpc, "some-thing-else")
     another_resource = Terrafying::Components::Instance.create_in(@vpc, "another-thing")
@@ -64,6 +97,60 @@ shared_examples "a usable resource" do
           rule[:to_port] == port[:number] && \
           rule[:protocol] == "tcp"
         }
+      }
+    ).to be true
+  end
+
+  it "should add icmp that maps the right resources" do
+    other_resource = Terrafying::Components::Instance.create_in(@vpc, "some-thing-else")
+
+    @main_resource.pingable_by(other_resource)
+
+    output = @main_resource.output_with_children
+
+    expect(output["resource"]["aws_security_group_rule"].count).to be >= 4
+
+    expect(
+      output["resource"]["aws_security_group_rule"].any? { |_, rule|
+        rule[:type] == "ingress" && \
+        rule.has_key?(:source_security_group_id) && \
+        rule[:source_security_group_id] == other_resource.egress_security_group && \
+        rule[:from_port] == 8 && \
+        rule[:to_port] == 0 && \
+        rule[:protocol] == 1
+      }
+    ).to be true
+
+    expect(
+      output["resource"]["aws_security_group_rule"].any? { |_, rule|
+        rule[:type] == "ingress" && \
+        rule.has_key?(:source_security_group_id) && \
+        rule[:source_security_group_id] == other_resource.egress_security_group && \
+        rule[:from_port] == 128 && \
+        rule[:to_port] == 0 && \
+        rule[:protocol] == 58
+      }
+    ).to be true
+
+    expect(
+      output["resource"]["aws_security_group_rule"].any? { |_, rule|
+        rule[:type] == "egress" && \
+        rule.has_key?(:source_security_group_id) && \
+        rule[:source_security_group_id] == @main_resource.ingress_security_group && \
+        rule[:from_port] == 8 && \
+        rule[:to_port] == 0 && \
+        rule[:protocol] == 1
+      }
+    ).to be true
+
+    expect(
+      output["resource"]["aws_security_group_rule"].any? { |_, rule|
+        rule[:type] == "egress" && \
+        rule.has_key?(:source_security_group_id) && \
+        rule[:source_security_group_id] == @main_resource.ingress_security_group && \
+        rule[:from_port] == 128 && \
+        rule[:to_port] == 0 && \
+        rule[:protocol] == 58
       }
     ).to be true
   end
