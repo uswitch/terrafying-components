cf043090c4bc4638fc886c870ca5894337f21c1e
Emils Solmanis <emils.solmanis@uswitch.com>
2018-06-07 16:37:17 +0100

    Tom says it'd work

--- lib/terrafying/components/letsencrypt.rb
+++ lib/terrafying/components/letsencrypt.rb
@@ -137,7 +137,7 @@ module Terrafying
         ctx.resource :aws_s3_bucket_object, "#{key_ident}-cert", {
                        bucket: @bucket,
                        key: File.join(@prefix, @name, name, "cert"),
-                       content: output_of(:acme_certificate, key_ident, :certificate_pem) + @ca_cert,
+                       content: output_of(:acme_certificate, key_ident, :certificate_pem) + '\n' + @ca_cert,
                      }
 
         reference_keypair(ctx, name)
