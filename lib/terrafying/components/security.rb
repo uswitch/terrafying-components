0efc6c59cd38b471b327dfe0b32a35c1d0072afd
Tom Booth <tombooth@gmail.com>
2019-05-22 13:00:51 +0100

    Add security components

---
+++ lib/terrafying/components/security.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'terrafying/components/security/config'
+require 'terrafying/components/security/config_aggregator'
+require 'terrafying/components/security/pagerduty_topic'
+require 'terrafying/components/security/store'
+require 'terrafying/components/security/trail'
---
+++ lib/terrafying/components/security/store.rb
@@ -0,0 +1,125 @@
+# frozen_string_literal: true
+
+require 'terrafying'
+
+module Terrafying
+
+  module Components
+
+    module Security
+
+      class Store < Terrafying::Context
+
+        attr_reader :name, :key_arn
+
+        def self.create(*args)
+          Store.new.create(*args)
+        end
+
+        def create(
+              name,
+              bucket_policy: nil,
+              key_policy: nil
+            )
+
+          ident = tf_safe(name)
+
+          @name = name
+          @key = resource :aws_kms_key, ident, { policy: key_policy }
+          @key_arn = @key["arn"]
+
+          resource :aws_kms_alias, ident, {
+                     name: "alias/#{name}",
+                     target_key_id: @key["id"],
+                   }
+
+          @bucket = resource :aws_s3_bucket, ident, {
+                               bucket: name,
+                               acl: "private",
+                               force_destroy: false,
+                               versioning: {
+                                 enabled: true,
+                               },
+                               policy: bucket_policy,
+                               server_side_encryption_configuration: {
+                                 rule: {
+                                   apply_server_side_encryption_by_default: {
+                                     kms_master_key_id: @key["arn"],
+                                     sse_algorithm: "aws:kms",
+                                   }
+                                 }
+                               },
+                               tags: {
+                                 Name: name,
+                               }
+                             }
+
+          self
+        end
+
+        def read_statements(prefix: "*")
+          bucket_glob = [@bucket["arn"], prefix].join("/")
+
+          [
+            {
+              Effect: "Allow",
+              Action: [
+                "s3:ListBucket",
+                "s3:GetBucketAcl",
+              ],
+              Resource: @bucket["arn"],
+            },
+            {
+              Effect: "Allow",
+              Action: [
+                "s3:GetObject*",
+              ],
+              Resource: bucket_glob,
+            },
+            {
+              Effect: "Allow",
+              Action: [
+                "kms:Decrypt",
+              ],
+              Resource: @key["arn"],
+            }
+          ]
+        end
+
+        def write_statements(prefix: "*")
+          bucket_glob = [@bucket["arn"], prefix].join("/")
+
+          [
+            {
+              Effect: "Allow",
+              Action: [
+                "s3:ListBucket",
+                "s3:GetBucketAcl",
+              ],
+              Resource: @bucket["arn"],
+            },
+            {
+              Effect: "Allow",
+              Action: [
+                "s3:GetObject*",
+                "s3:PutObject*",
+              ],
+              Resource: bucket_glob,
+            },
+            {
+              Effect: "Allow",
+              Action: [
+                "kms:Encrypt",
+                "kms:Decrypt",
+                "kms:GenerateDataKey"
+              ],
+              Resource: @key["arn"],
+            }
+          ]
+        end
+
+      end
+
+    end
+  end
+end
