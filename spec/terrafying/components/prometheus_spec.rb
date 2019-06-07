c40b6ee4389bcf3ec0aa18d9b981b3f793957e30
DewaldV <dewald.viljoen@uswitch.com>
2019-06-07 11:06:51 +0100

    Add data volumes to the prometheus instances
    This will allow them to be rotated and keep their current data

--- spec/terrafying/components/prometheus_spec.rb
+++ spec/terrafying/components/prometheus_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe Terrafying::Components::Prometheus, '#find_in' do
   let(:aws) { double('AWS') }
   let(:vpc) { stub_vpc('test-vpc', '10.10.0.0/16', aws: aws) }
 
+  before do
+    allow(aws).to receive(:instance_type_vcpu_count).and_return(2)
+  end
+
   context 'finding prom for a vpc' do
     it 'should return a prom with a security group if one exists' do
       allow(aws).to receive(:security_group_in_vpc).and_return('sg-1234567890')
@@ -13,6 +17,16 @@ RSpec.describe Terrafying::Components::Prometheus, '#find_in' do
       prom = described_class.find_in(vpc: vpc)
 
       expect(prom.security_group).to be('sg-1234567890')
+    end
+  end
+
+  context 'storing promtheus data' do
+    it 'should add a volume to store the data with a default size of 20 GB' do
+      prom = described_class.create_in(vpc: vpc)
+
+      prom_vol = prom.output_with_children['resource']['aws_ebs_volume']['prometheus-0-0']
+      puts prom_vol
+      expect(prom_vol).to include(size: 20)
     end
   end
 end
